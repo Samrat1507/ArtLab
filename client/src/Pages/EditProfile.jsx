cf81c57cbf4daf936cf2b7e39d591b116357865b
Jayanti2919 <jayanti2919@gmail.com>
2023-06-02 04:53:10 +0530

    added editprofile

--- client/src/Pages/EditProfile.jsx
+++ client/src/Pages/EditProfile.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const EditProfile = () => {
-  return (
-    <div>EditProfile</div>
-  )
-}
 
-export default EditProfile
+  const [user, setUser] = useState('')
+
+  useEffect(() => {
+    async function validate() {
+      const token = sessionStorage.getItem("userToken");
+      const res = await fetch("http://localhost:5000/user/auth", {
+        method: "GET",
+        headers: {
+          "x-access-token": token,
+        },
+      });
+
+      const data = await res.json();
+      if (data.status != 401) console.log(data);
+      else {
+        console.log(data.status);
+        nav("/login");
+      }
+    }
+  });
+  return <div>EditProfile</div>;
+};
+
+export default EditProfile;
